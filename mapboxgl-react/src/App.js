55bc9af659490b053a0813353af252272939fbe6
Scott Davis <stdavis@utah.gov>
2017-09-22 09:41:31 -0600

    implement map and radio buttons

--- mapboxgl-react/src/App.js
+++ mapboxgl-react/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RkYXZpcyIsImEiOiI5Mkdac0FNIn0.GEQLOdntbI01q3JeFkQuTg';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {mapVisible: true};
+  }
+  componentDidMount() {
+    new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/outdoors-v10',
+      center: [-111.8, 40.55],
+      zoom: 8
+    });
+  }
+  toggle(evt) {
+    this.setState({mapVisible: evt.target.value === 'map'});
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='app'>
+        <label>
+          <input type='radio' value='map' name='toggle' checked={this.state.mapVisible} onChange={this.toggle.bind(this)}></input>
+          map
+        </label>
+        <label>
+          <input type='radio' value='list' name='toggle' checked={!this.state.mapVisible} onChange={this.toggle.bind(this)}></input>
+          list
+        </label>
+        <div ref={el => this.mapContainer = el}></div>
       </div>
     );
   }
